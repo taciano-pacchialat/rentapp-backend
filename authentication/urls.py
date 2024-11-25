9580eb6a51e4629f6942e9df028cf47c645d96e1
Taciano <taciano3003@gmail.com>
2024-11-24 21:34:36 -0300

    fetch user by email works

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import RegisterView
+from .views import RegisterView, UserViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('login/', obtain_auth_token, name='auth_login'),
+    path('', include(router.urls))
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets, permissions, filters
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from .models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserSerializer
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['email']
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
