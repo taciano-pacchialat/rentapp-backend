5c3009e7188acfc863bcebb641bc029c90b314d2
Taciano <taciano3003@gmail.com>
2024-11-25 13:07:38 -0300

    ajustes en imagenes

--- apartments/serializers.py
+++ apartments/serializers.py
@@ -10,7 +10,7 @@ class ApartmentImageSerializer(serializers.ModelSerializer):
 
 class ApartmentSerializer(serializers.ModelSerializer):
     owner = UserSerializer(read_only=True)
-    images = ApartmentImageSerializer(source='apartment_images', many=True, read_only=True)
+    images = ApartmentImageSerializer(many=True, required=False, read_only=True)
     
     floor = serializers.IntegerField(required=False, allow_null=True)
     letter = serializers.CharField(required=False, allow_blank=True, allow_null=True)
@@ -20,14 +20,25 @@ class ApartmentSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
-        # Extract images from the request
         request = self.context.get('request')
         images = request.FILES.getlist('images')
 
-        # Create the Apartment instance
         apartment = Apartment.objects.create(**validated_data)
 
-        # Create ApartmentImage instances
         for image in images:
             ApartmentImage.objects.create(apartment=apartment, image=image)
         return apartment
+
+    def update(self, instance, validated_data):
+        request = self.context.get('request')
+        images = request.FILES.getlist('images')
+
+        # Update apartment fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        # Add new images
+        for image in images:
+            ApartmentImage.objects.create(apartment=instance, image=image)
+        return instance
